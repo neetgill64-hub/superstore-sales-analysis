596222a9fd0882e0447efeb0780c1e8a004dde0f
neetgill64-hub <neetgill64@gmail.com>
2026-07-30 15:09:01 +1200

    Update superstore_analysis.sql

--- superstore_analysis.sql
+++ superstore_analysis.sql
@@ -1,1 +1,6 @@
+# Total Sale for Superstore
+SELECT * FROM `superstore-503204.sales_analysis.cleaned_superstore_sales` LIMIT 1000;
 
+SELECT
+    SUM(total) AS total_sales
+FROM `superstore-503204.sales_analysis.cleaned_superstore_sales`;
